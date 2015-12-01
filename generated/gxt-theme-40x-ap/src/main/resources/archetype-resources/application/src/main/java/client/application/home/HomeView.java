e46e586c1c1b3fe47c3a776c63220f00005736cb
branflake2267@gmail.com <branflake2267@gmail.com>
2015-11-30 16:05:09 -0800

    Regenerated gxt-basic-support-3x

---
+++ generated/gxt-theme-40x-ap/src/main/resources/archetype-resources/application/src/main/java/client/application/home/HomeView.java
@@ -0,0 +1,39 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client.application.home;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
+
+import ${package}.client.application.Presenter;
+
+public class HomeView implements IsWidget {
+
+  private Presenter presenter;
+
+  private VerticalLayoutContainer widget;
+
+  public void setPresenter(Presenter presenter) {
+    this.presenter = presenter;
+  }
+
+  public void start() {
+
+  }
+
+  @Override
+  public Widget asWidget() {
+    if (widget == null) {
+      widget = new VerticalLayoutContainer();
+
+      widget.add(new HTML("Home"), new VerticalLayoutData(1, 1));
+    }
+
+    return widget;
+  }
+
+}
---
+++ generated/gxt-theme-40x-ap/src/main/resources/archetype-resources/theme/src/main/java/theme/tritium/custom/client/slider/TritonHorizontalSliderAppearance.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.theme.tritium.custom.client.slider;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+
+import ${package}.theme.tritium.client.base.slider.Css3HorizontalSliderAppearance;
+
+public class TritonHorizontalSliderAppearance extends Css3HorizontalSliderAppearance {
+
+  public interface TritonHorizontalSliderResources extends Css3HorizontalSliderResources {
+
+    @Source({"${packageInPathFormat}/theme/tritium/client/base/slider/Css3HorizontalSlider.gss",
+            "TritonSlider.gss", "TritonHorizontalSlider.gss"})
+    TritonHorizontalSliderStyle style();
+
+    @ImageOptions(width = 20, height = 20)
+    ImageResource sliderThumbHorizontal();
+  }
+
+  public interface TritonHorizontalSliderStyle extends Css3HorizontalSliderStyle {
+  }
+
+  public TritonHorizontalSliderAppearance() {
+    this(GWT.<TritonHorizontalSliderResources> create(TritonHorizontalSliderResources.class));
+  }
+
+  public TritonHorizontalSliderAppearance(Css3HorizontalSliderResources resources) {
+    super(resources);
+  }
+}
