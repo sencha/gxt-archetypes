2cbd1efcaca4ae8319cb4565f16112376cb46b1a
branflake2267@gmail.com <branflake2267@gmail.com>
2016-12-16 11:26:24 -0800

    Adding gwtp and mvp4g archetypes.

---
+++ archetypes/gxt-theme-40x-gwtp/application/src/main/java/tld/domain/project/client/application/home/HomeUiHandlers.java
@@ -0,0 +1,11 @@
+package tld.domain.project.client.application.home;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+
+interface HomeUiHandlers extends UiHandlers {
+
+  void onOpenWindow(String name);
+
+  void onOpenGrid();
+
+}
