90f7cade323f333e2e6d303e4b6e0b492977d351
Brandon Donnelson <branflake2267@gmail.com>
2019-02-13 12:39:05 -0800

    Remove generated directory

--- generated/gxt-theme-40x-gwtp/src/main/resources/archetype-resources/application/src/main/java/client/application/widgets/windowwidget/WindowWidgetUiHandlers.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.client.application.widgets.windowwidget;
-import com.gwtplatform.mvp.client.UiHandlers;
-
-public interface WindowWidgetUiHandlers extends UiHandlers {
-    
-}
